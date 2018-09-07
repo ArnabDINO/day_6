fe3ac3af61df0a1a7cf1049b05f6fd3fce3db53b
ArnabDINO <42172403+ArnabDINO@users.noreply.github.com>
2018-09-07 13:48:34 +0530

    Update lab6_q4.cpp

--- lab6_q4.cpp
+++ lab6_q4.cpp
@@ -6,13 +6,13 @@ int main()
 	int n;
 	cout<<"Give the number of stars in one rows of the rhombus"<<endl;
 	cin>>n;
-	for(int i=0;i<n;i++)
+	for(int i=0;i<n;i++)//for changing the rows
 	{
-		for(int j=(n-i);j>0;j--)
+		for(int j=(n-i);j>0;j--)//loop for spaces
 		{
 			cout<<" ";
 		}
-		for(int j=0;j<n;j++)
+		for(int j=0;j<n;j++)//loop for the stars
 		{
 			cout<<"*";
 		}
