a5ca2e33d1c86e7e70e2daaaef8390e5cdd27313
ArnabDINO <42172403+ArnabDINO@users.noreply.github.com>
2018-09-07 14:29:05 +0530

    Update lab6_q5.cpp

--- lab6_q5.cpp
+++ lab6_q5.cpp
@@ -6,15 +6,15 @@ int main()
 	int n;
 	cout<<"give me number of stars i will give you hollow rhombus"<<endl;
 	cin>>n;
-	for(int i=0;i<n;i++)
+	for(int i=0;i<n;i++)//loop for changing the pwr(present working row :-))
 	{
-		if (i==0||i==(n-1))
+		if (i==0||i==(n-1))//for the first and last row; as there will be full n number of star in these two rows 
 		{
-			for(int j=(n-i);j>0;j--)
+			for(int j=(n-i);j>0;j--)//for spaces
 			{
 				cout<<" ";
 			}
-			for(int j=0;j<n;j++)
+			for(int j=0;j<n;j++)//for the whole line of stars
 			{
 				cout<<"*";
 			}
@@ -22,11 +22,11 @@ int main()
 		}
 		else
 		{
-			for(int j=(n-i);j>0;j--)
+			for(int j=(n-i);j>0;j--)//the spaces return and they are decreasing with increasing lines
 			{
 				cout<<" ";
 			}
-			for(int j=0;j<n;j++)
+			for(int j=0;j<n;j++)//the stars return too but they are same as the hollow square program 
 			{
 				if(j==0||j==(n-1))
 				{
