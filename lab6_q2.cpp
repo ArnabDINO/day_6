6e38d07d8abe75d8a6c6c087b094cbc7ebebbd35
ArnabDINO <42172403+ArnabDINO@users.noreply.github.com>
2018-09-07 13:42:26 +0530

    Update lab6_q2.cpp

--- lab6_q2.cpp
+++ lab6_q2.cpp
@@ -6,21 +6,21 @@ int main()
 	int n;
 	cout<<"give the number of stars in each side of the square"<<endl;
 	cin>>n;
-	for(int i=0;i<n;i++)
+	for(int i=0;i<n;i++)//loop for printing different rows
 	{
-		if (i==0||i==(n-1))
+		if (i==0||i==(n-1))//condition for selecting only the first and last line
 		{
-			for (int j=0;j<n;j++)
+			for (int j=0;j<n;j++)//for printing out the elements in each of the first and last line
 			{
 				cout<<"*";
 			}
 			cout<<endl;
 		}
-		else
+		else//for the lines in between where there will be only two stars: one in the first place and the other in the last place
 		{
 			for (int j=0;j<n;j++)
 			{
-				if(j==0||j==(n-1))
+				if(j==0||j==(n-1))//condition for selecting the first and last elements of each row
 				{
 					cout<<"*";
 				}
