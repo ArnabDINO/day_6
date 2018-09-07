31a02e213a7fcd3ac06f26148df21f198281d056
ArnabDINO <42172403+ArnabDINO@users.noreply.github.com>
2018-09-07 14:05:01 +0530

    Update lab6_q9.cpp

--- lab6_q9.cpp
+++ lab6_q9.cpp
@@ -7,9 +7,10 @@ int main()
 	cout<<"give me n i'll give you hollow triangle"<<endl;
 	cin>>n;
 	
-		for (int j=0;j<=n;j++)
+		for (int j=0;j<=n;j++)//as usual loop for changing the current row
 		{
-			if(j==0||j==(n))
+			if(j==0||j==(n))//condition for first and last row
+				//the first row could have been neglected but for symmetry purpose it has been included in the condition
 			{
 				for(int l=0;l<=j;l++)
 				{
@@ -17,7 +18,10 @@ int main()
 				}
 				cout<<endl;
 			}
-			else
+			else//for other cases the program will be like the hollow sqare one.
+				//but in the hollow square value of the counter variable had been running from 0 to n
+				//here value of l will run from 0 to j 
+				//so the result will be triangle
 			{
 				for(int l=0;l<=j;l++)
 				{
