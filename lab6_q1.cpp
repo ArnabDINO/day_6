64a628bef4a0c2d347245840c634375a296f840c
ArnabDINO <42172403+ArnabDINO@users.noreply.github.com>
2018-09-07 13:38:15 +0530

    Update lab6_q1.cpp

--- lab6_q1.cpp
+++ lab6_q1.cpp
@@ -6,9 +6,9 @@ int main()
 	int n,j;
 	cout<<"give the number of stars in each side"<<endl;
 	cin>>n;
-	for (int i=0;i<n;i++)
+	for (int i=0;i<n;i++)//loop for the rows
 	{
-		for(int j=0;j<n;j++)
+		for(int j=0;j<n;j++)//loop for printing every elements in a row
 		{
 			cout<<"*";
 		}
