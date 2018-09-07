ab3cf33aab01f4cd51cf48843d72a0448cacca3a
ArnabDINO <42172403+ArnabDINO@users.noreply.github.com>
2018-09-07 13:53:03 +0530

    Update lab6_q8.cpp

--- lab6_q8.cpp
+++ lab6_q8.cpp
@@ -6,9 +6,10 @@ int main()
 	int n;
 	cout<<"give the number of stars in base"<<endl;
 	cin>>n;
-	for (int i=0;i<n;i++)
+	for (int i=0;i<n;i++)//loop for changing the row
 	{
-		for (int j=0;j<=i;j++)
+		for (int j=0;j<=i;j++)//loop for printing increasing number of stars for each successive rows.
+			//the upper limit of the possible value of j increaces with each iteration as i is increasing too after each cycle.
 		{
 			cout<<"*";
 		}
