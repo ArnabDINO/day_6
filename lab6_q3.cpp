6def73c137db067e1b026001745c7e41569204fb
ArnabDINO <42172403+ArnabDINO@users.noreply.github.com>
2018-09-07 13:46:48 +0530

    Update lab6_q3.cpp

--- lab6_q3.cpp
+++ lab6_q3.cpp
@@ -6,11 +6,11 @@ int main()
 	int n;
 	cout <<"Give the number of stars in each side of the square"<<endl;
 	cin>> n;
-	for (int i=0;i<n;i++)
+	for (int i=0;i<n;i++)//for printing multiple rows
 	{
-		if (i==0||i==(n-1))
+		if (i==0||i==(n-1))//condition for first and last row of the square
 		{
-			for (int j=0;j<n;j++)
+			for (int j=0;j<n;j++)//for printing out the elements of a row
 			{
 				cout<<"*";
 			}
@@ -18,9 +18,9 @@ int main()
 		}
 		else
 		{
-			for(int j=0;j<n;j++)
+			for(int j=0;j<n;j++)//for printing out the elements of a row
 			{
-				if(j==i||j==(n-1-i)||j==0||j==(n-1))
+				if(j==i||j==(n-1-i)||j==0||j==(n-1))//selection of the for positions in each row where the stars will printed
 				{
 					cout<<"*";
 				}
